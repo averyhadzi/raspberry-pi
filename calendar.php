870696cec8485831fc4805784604f454d851c5c1
Avery Wolkin Hadzi <averywolkin@Macintosh-3.local>
2014-07-31 15:07:05 -0600

    first commit

---
+++ calendar.php
@@ -0,0 +1,3 @@
+<?php
+	$url = 'https://www.google.com/calendar/ical/35a1hb7smvh1omgsf78ti3gh80%40group.calendar.google.com/private-f9f622ab780fa31f0c455c61d1418862/basic.ics';
+	echo file_get_contents($url);
